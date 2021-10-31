28bc7f12e14a99ed68d57b83f0e297a78d191ffe
AlexDegterov <degterov@proektant.by>
2021-10-31 23:58:54 +0300

    Fixed showing pictures and added the lint to cart on the beanstalk

--- src/constants/api-paths.ts
+++ src/constants/api-paths.ts
@@ -3,5 +3,5 @@ export const API_PATHS = {
 	order: 'https://4eumx86rj2.execute-api.eu-west-1.amazonaws.com/dev',
 	import: 'https://td4poxjw2k.execute-api.eu-west-1.amazonaws.com/dev',
 	bff: 'https://4eumx86rj2.execute-api.eu-west-1.amazonaws.com/dev',
-	cart: 'https://4eumx86rj2.execute-api.eu-west-1.amazonaws.com/dev',
+	cart: 'http://development.eba-fdivfpkm.eu-west-1.elasticbeanstalk.com/api',
 };
